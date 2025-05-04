8164348f1d5bc1dd929e040a8d6c54603f4618ef
Vikramsathish <85689307+Vikramsathish@users.noreply.github.com>
2025-05-04 21:03:31 +0530

    cart count feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import Button from './UI/Button.jsx';
 import logoimg from '../assets/logo.jpg';
+import { useContext } from 'react';
+import CartContext from '../store/CartContext.jsx';
 
 export default function Header(){
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+    console.log(totalCartItems);
     return <header id="main-header">
         <div id="title">
             <img src={logoimg} alt="A restaurant"/>
             <h1>Zesty Eats</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 };
