c00e684161ffae89dd0806516a5eb239e1b2a7cd
Vikramsathish <85689307+Vikramsathish@users.noreply.github.com>
2025-06-01 15:27:46 +0530

    FOA-m-01

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from '../util/formatting.js';
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
-
+import CartItem from "./CartItem.jsx";
 
 export default function Cart(){
     const cartCtx = useContext(CartContext);
@@ -23,15 +23,19 @@ export default function Cart(){
         <h2>Your Cart</h2>
         <ul>
         {cartCtx.items.map((item) => (
-            <li key={item.id}>
-                {item.name} - {item.quantity}
-            </li>
+            <CartItem
+                key={item.id}
+                name={item.name}
+                quantity={item.quantity}
+                price={item.price}
+                onIncrease={() => cartCtx.addItem(item)}
+                onDecrease={() => cartCtx.removeItem(item.id)}
+            />
         ))}
         </ul>
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
-            <Button textOnl onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go to Checkout</Button>
+                <Button textOnl onClick={handleCloseCart}>Close</Button>
         </p>
     </Modal>
     );
